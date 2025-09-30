ef4edb09012734593c0c7ae5a17bed8a43910a9c
Alejandro Llera Martin <allera-m@c2r15s1.42madrid.com>
2025-09-30 11:21:54 +0200

    ex02 done

---
+++ ex02/Base.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Base.hpp"
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+Base* generate(void) {
+    int r = rand() % 3;
+    switch (r) {
+        case 0: std::cout << "Generated A\n"; return new A;
+        case 1: std::cout << "Generated B\n"; return new B;
+        default: std::cout << "Generated C\n"; return new C;
+    }
+}
+
+void identify(Base* p) {
+    if (dynamic_cast<A*>(p)) std::cout << "A\n";
+    else if (dynamic_cast<B*>(p)) std::cout << "B\n";
+    else if (dynamic_cast<C*>(p)) std::cout << "C\n";
+}
+
+void identify(Base& p) {
+    try { (void)dynamic_cast<A&>(p); std::cout << "A\n"; return; }
+    catch(...) {}
+    try { (void)dynamic_cast<B&>(p); std::cout << "B\n"; return; }
+    catch(...) {}
+    try { (void)dynamic_cast<C&>(p); std::cout << "C\n"; return; }
+    catch(...) {}
+}
+
+int main() {
+    srand(time(0));
+
+    Base* obj = generate();
+    identify(obj);         
+    identify(*obj);        
+    delete obj;
+}
